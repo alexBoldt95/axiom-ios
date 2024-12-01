37cbeb9209c873cba7dca3ef9d4415a319056663
Alex Boldt <alexboldt95@gmail.com>
2024-12-01 16:45:21 -0600

    Initial Commit

---
+++ axiom-ios macOS/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  axiom-ios macOS
+//
+//  Created by Alexander Boldt on 12/1/24.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class GameViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let scene = GameScene.newGameScene()
+        
+        // Present the scene
+        let skView = self.view as! SKView
+        skView.presentScene(scene)
+        
+        skView.ignoresSiblingOrder = true
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+    }
+
+}
+
